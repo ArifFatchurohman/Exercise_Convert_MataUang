8b70cd9eb75653e44cc347a7ceb9c675893bfd15
LENOVO SLIM 3 <LENOVO SLIM 3@LAPTOP-3QOIRAE6>
2022-01-04 18:42:28 +0700

    exercise convert mata uang

---
+++ src/main/java/Exercise1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LENOVO SLIM 3
+ */
+public class Exercise1 {
+    public static void main(String[] args){
+    double rupiah = 14000;
+    double dollar;
+ 
+    dollar = rupiah / 14000;
+
+    System.out.println("Konverensi ke dollar = " + dollar);
+ 
+  }
+}
---
+++ src/main/java/Perulangan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LENOVO SLIM 3
+ */
+public class Perulangan {
+    public static void main (String[] args)
+    {  
+        System.out.println ("Pengulangan Angka Naik");
+        for (int i = 13; i <= 100; ++i)
+        System.out.println ("Angka ke = "+i);
+        System.out.println ("Pengulangan Angka Turun");
+        for (int i = 100; i > 12; --i)
+        System.out.println ("Angka ke = "+i);
+    }
+}
